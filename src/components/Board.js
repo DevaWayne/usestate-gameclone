e55983a43394c4319144ff7f0fbc200ce9a42642
Sri Devandren <sdevandren@gmail.com>
2021-04-01 16:52:39 +0800

    correction

--- src/components/Board.js
+++ src/components/Board.js
@@ -87,19 +87,16 @@ const Board = () => {
     return (
         <div>
           <div className="status">{status}</div>
-          //row 1
           <div className="board-row">
             {renderSquare(0)}
             {renderSquare(1)}
             {renderSquare(2)}
           </div>
-          //row 2
           <div className="board-row">
             {renderSquare(3)}
             {renderSquare(4)}
             {renderSquare(5)}
           </div>
-          //row 3
           <div className="board-row">
             {renderSquare(6)}
             {renderSquare(7)}
